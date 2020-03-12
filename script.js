fa32d112a70caab10dddb3dd5fdd532cd5b84a27
D3viii <devinsdevs@gmail.com>
2020-03-11 20:38:06 -0400

    Creating Varaibles

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 
 
-console.log(questions4)
+var quest = document.getElementById("#question")
+var questions = [" What is my name?", " What program do we use to interact with the class? ", " What time does class start? "]
+
+
+var answers0 = ["A: kevin", "B: tyler", "C: Devin"]
+var answers1 = ["A: Twitter", "B: Slack", "C: Instagram"]
+
+console.log(questions[0])
+console.log(answers0)
+
+
+
+
+
+
