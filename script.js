2bb05443ec51f6a70612f54321e8a2ea4e44a997
D3viii <devinsdevs@gmail.com>
2020-03-11 21:51:22 -0400

    Final Commit

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 
 var quest = document.getElementById("#question")
 var questions = [" What is my name?", " What program do we use to interact with the class? ", " What time does class start? "]
+var button1 = document.getElementById("#Answer1")
+var button2 = document.getElementById("#Answer2")
+var button3 = document.getElementById("#Answer3")
 
+var buttons = document.getElementsByClassName("btn btn-outline-primary")
 
 var answers0 = ["A: kevin", "B: tyler", "C: Devin"]
 var answers1 = ["A: Twitter", "B: Slack", "C: Instagram"]
+var answers2 = ["A: 9", "B: 10", "C: 12"]
+var timer = timer
 
-console.log(questions[0])
-console.log(answers0)
+
+function quizBody(){
+    var quiz1 = questions[0] + answers0.button1
+    if (questions[0] = answers0[2]){
+        console.log(quiz1)
+
+    }else {
+        timer--
+    }
+    quizBody()
+}
+
+
+
 
 
 
